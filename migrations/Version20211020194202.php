a4e8f7515e5b68f0f3324f8a9c5944da0f963996
mskraban <mario.skraban@gmail.com>
2021-10-21 01:26:49 +0200

    Updated migration table

--- migrations/Version20211020194202.php
+++ migrations/Version20211020194202.php
@@ -20,16 +20,15 @@ final class Version20211020194202 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE flaviar (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE flaviar_order');
-        $this->addSql('ALTER TABLE `order` ADD send_to VARCHAR(255) NOT NULL, ADD reciever VARCHAR(255) NOT NULL, ADD message VARCHAR(255) NOT NULL, ADD sender VARCHAR(255) NOT NULL, ADD membership VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD sender_select VARCHAR(255) NOT NULL, ADD reciever VARCHAR(255) NOT NULL, ADD message VARCHAR(255) NOT NULL, ADD sender VARCHAR(255) NOT NULL, ADD membership VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE flaviar_order (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE flaviar');
-        $this->addSql('ALTER TABLE `order` DROP send_to, DROP reciever, DROP message, DROP sender');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE order');
+        $this->addSql('ALTER TABLE `order` DROP sender_select, DROP reciever, DROP message, DROP sender, DROP membership');
     }
 }
